f2b64936f1a57ea1e711788d6ed0c971a4f8aa9d
Irene3013 <i.etxag@gmail.com>
2024-10-16 11:06:28 +0200

    train rewrite

--- erantzunak_berridatzi.py
+++ erantzunak_berridatzi.py
@@ -1,7 +1,8 @@
 import transformers
 import torch
 import argparse
-
+import json
+import os
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -10,6 +11,9 @@ def parse_args():
         "--model_type", type=str, required=True, choices=["openchat", "llama-8b"],
         help="Model type to be fine-tuned."
     )
+    parser.add_argument(
+        "--root", type=str, default="/gaueko0/users/ietxarri010/GrAL_Irene/okvqa", help="Path to the OkVqa prediction files."
+    )
 
     args = parser.parse_args()
     return args
@@ -17,6 +21,24 @@ def parse_args():
 def main():
     print("Parsing args...")
     args = parse_args()
+
+    # Load jsons
+    with open(os.path.join(args.root, 'train', f'annotations_train.json'), "r") as f:
+            train = json.load(f)
+
+    #with open(os.path.join(args.root, 'val', f'annotations_val.json'), "r") as f:
+    #        val = json.load(f)
+
+    # Create output files
+    train_values = f"./output/values/train_values.txt"
+    #val_values = f"./output/values/train_values.txt"
+
+    os.makedirs(os.path.dirname(train_values), exist_ok=True)
+    #os.makedirs(os.path.dirname(val_values), exist_ok=True)
+
+    with open(train_values, 'w') as f: pass
+    #with open(val_values, 'w') as f: pass
+       
 
     if args.model_type == "llama-8b":
         model_id = "meta-llama/Meta-Llama-3.1-8B-Instruct"
@@ -39,11 +61,20 @@ def main():
     elif args.model_type == "openchat":
         pipeline = transformers.pipeline("text-generation", model="openchat/openchat-3.5-0106", torch_dtype=torch.bfloat16, device_map="auto")
 
-        prompt = "¿When was America discovered?"
+        for ann in train:
+            question = ann['question']
+            answers = ann['answer']
 
-        result = pipeline(prompt, max_length=50, num_return_sequences=1) 
+            with open(train_values, 'a') as f:
+                 f.write(f'{question}')
+            
+            for answer in answers:
+                prompt = f"Question: {question}\nAnswer: {answer}\nPlease rewrite the answer using synonyms or rephrasing."
+                result = pipeline(prompt, max_length=50, truncation=True, num_return_sequences=1) 
+                #print(result[0]['generated_text'], "\n")
 
-        print(result[0]['generated_text'], "\n")
+                with open(train_values, 'a') as f:
+                    f.write(f' {answer} --> {result[0]['generated_text']}')
 
 if __name__ == "__main__":
     main()
