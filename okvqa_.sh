48db22eef7ca5d31b2082dcc30cfb88f99a082aa
Irene3013 <i.etxag@gmail.com>
2024-10-17 19:50:11 +0200

    berridatzi

--- okvqa_.sh
+++ okvqa_.sh
@@ -10,8 +10,4 @@
 source /gscratch/users/asalaberria009/env/p39-cu115/bin/activate
 
 #export TRANSFORMERS_CACHE="/gaueko0/transformers_cache/"
-
-srun python mm_okvqa_finetuning.py --model "OFA-Sys/ofa-base" --target_model ofa --location_encoding none \
-   --lr 2e-5 --batch_size 4 --max_steps 20000 --accumulate_grad_batches 2 \
-   --run_name ofa_base_okvqa --train --evaluate --source vinvl \
-   --root /gaueko0/users/ietxarri010/GrAL_Irene/okvqa
+srun python erantzunak_berridatzi.py --model_type "llama-8b" --root /gaueko0/users/ietxarri010/GrAL_Irene/okvqa
