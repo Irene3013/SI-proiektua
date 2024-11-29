37051643f7f206626ea46fded75f161978e95a00
Irene3013 <i.etxag@gmail.com>
2024-11-29 10:47:41 +0100

    aldaketak1

--- ofa_mc1_okvqa.sh
+++ ofa_mc1_okvqa.sh
@@ -16,4 +16,8 @@ srun python mm_okvqa_finetuning.py --model "OFA-Sys/ofa-base" --target_model ofa
    --lr 2e-5 --batch_size 4 --max_steps 20000 --accumulate_grad_batches 2 \
    --run_name ofa_base_okvqa_mc1 --train --evaluate --source vinvl --dataset mc1\
    --root /gaueko0/users/ietxarri010/GrAL_Irene/okvqa
-g
+
+srun python mm_okvqa_finetuning.py --model "OFA-Sys/ofa-base" --target_model ofa --location_encoding none \
+   --lr 2e-5 --batch_size 4 --max_steps 20000 --accumulate_grad_batches 2 \
+   --run_name ofa_base_okvqa_mc1_syn --train --evaluate --source vinvl --dataset mc1 --synonyms\
+   --root /gaueko0/users/ietxarri010/GrAL_Irene/okvqa
